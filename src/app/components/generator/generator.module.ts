368cfe14ec85c54e865cf3122f28bad46b5beb13
Zitelli Gabriel <gabriel.luciano.zitelli@gmail.com>
2022-07-05 23:00:41 -0300

    feat add component to handle generators

--- src/app/components/generator/generator.module.ts
+++ src/app/components/generator/generator.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LinealComponent } from './lineal/lineal.component';
 import { MultiplicativeComponent } from './multiplicative/multiplicative.component';
 import { JavascriptComponent } from './javascript/javascript.component';
+import { GeneratorComponent } from './generator.component';
 
 
 @NgModule({
   declarations: [
     LinealComponent,
     MultiplicativeComponent,
-    JavascriptComponent
+    JavascriptComponent,
+    GeneratorComponent
   ],
   imports: [
     CommonModule,
